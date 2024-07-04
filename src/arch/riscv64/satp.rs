1abb68bc96dc33890271813acec6d60df4a3ef94
ZhiyuanSue <108735103+ZhiyuanSue@users.noreply.github.com>
2024-07-04 17:15:57 +0800

    Sel4_Common and aarch64 partial support (#26)
    * try to sort the arch_init_freemem
    
    * update the cache code
    
    * try to change the common registers
    
    * fix the prev bug
    
    * add something in aarch64 platform
    
    * try to fix the prev bug

--- src/arch/riscv64/satp.rs
+++ src/arch/riscv64/satp.rs
@@ -44,6 +44,7 @@ pub fn sfence_local() {
 #[cfg(not(feature = "ENABLE_SMP"))]
 #[inline]
 pub fn sfence() {
+    #[cfg(target_arch = "riscv64")]
     unsafe {
         core::arch::asm!("sfence.vma");
     }
